18ea82ef0e9054a296f3a3b0098b4ffbcd1b420f
ShivamKhator <iam.shivamkhator@gmail.com>
2025-12-27 13:02:21 +0530

    Add: Adapter to PrismaClient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,27 +2,20 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-// 1. Setup the PG Pool
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Use your environment variable
 const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL 
 });
-
-// 2. Initialize the Prisma Adapter
 const adapter = new PrismaPg(pool);
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
-
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+  globalForPrisma.prisma ?? 
+  new PrismaClient({ 
+    adapter, // THIS IS THE KEY REQUIREMENT IN V7
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
