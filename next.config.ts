3c05e096fff4e100390e7eea32ba917c4be7ab64
ShivamKhator <iam.shivamkhator@gmail.com>
2025-12-30 21:26:10 +0530

    Fix: pwa incompatibility

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,21 @@
-import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+import type { NextConfig } from "next"
+import withPWA from "next-pwa"
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
   images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "skybee.vercel.app"
-    },
-    {
-      protocol: "https",
-      hostname: "*.googleusercontent.com"
-    },
-    {
-      protocol: "https",
-      hostname: "images.pexels.com",
-    }
-  ],
+    remotePatterns: [
+      { protocol: "https", hostname: "skybee.vercel.app" },
+      { protocol: "https", hostname: "*.googleusercontent.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+    ],
+  },
 }
-};
 
 export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-})(nextConfig)
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig as any)
