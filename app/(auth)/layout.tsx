bf1128fad7bd4254d7dde45b158719d5599c00f1
ShivamKhator <iam.shivamkhator@gmail.com>
2026-01-14 23:48:15 +0530

    Fixes to login pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Terriva",
@@ -26,7 +15,7 @@ export default function AuthLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <SessionProviderWrapper>
           {children}
